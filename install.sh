3ed2c0b4ddfbf9554564c9650db1a07b1f12192f
ardxryz <ardhitahosting5@gmail.com>
2024-10-14 21:02:35 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+# Color
+BLUE='\033[0;34m'       
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+# Display welcome message
+display_welcome() {
+  echo -e ""
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]                                                 [+]${NC}"
+  echo -e "${BLUE}[+]                AUTO INSTALLER THEMA             [+]${NC}"
+  echo -e "${BLUE}[+]                  © HANMOD YT                [+]${NC}"
+  echo -e "${BLUE}[+]                                                 [+]${NC}"
+  echo -e "${RED}[+] =============================================== [+]${NC}"
+  echo -e ""
+  echo -e "script ini di buat untuk mempermudah penginstalasian thema pterodactyl,"
+  echo -e "dilarang keras untuk memperjual belikan."
+  echo -e ""
+  echo -e "𝗖𝗥𝗘𝗗𝗜𝗧𝗦 :"
+  echo -e "@Hanmod"
+  sleep 4
+  clear
+}
+
+#Check user token
+check_token() {
+  echo -e "                                                       "
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "${BLUE}[+]                 LICENSY HANMOD YT                 [+]${NC}"
+  echo -e "${BLUE}[+] =============================================== [+]${NC}"
+  echo -e "                                                       "
+  TOKEN=$(jq -r '.token' token.json)
+
+  echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
+  read -r USER_TOKEN
+
+  if [ "$USER_TOKEN" = "hanmodyt" ]; then
+    echo -e "${GREEN}AKSES BERHASIL${NC}}"
+  else
+    echo -e "${GREEN}Ini Thema Panel Pribadi Nya Hanmod Tau${NC}"
+    exit 1
+  fi
+  clear
+}
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+clear
+
+installTheme(){
+    cd /var/www/
+    tar -cvf Pterodactyl_Ardhita_Themebackup.tar.gz pterodactyl
+    echo "Installing theme..."
+    cd /var/www/pterodactyl
+    rm -r Pterodactyl_Ardhita_Theme
+    git clone https://github.com/ardxryz/Pterodactyl_Ardhita_Theme.git
+    cd Pterodactyl_Ardhita_Theme
+    rm /var/www/pterodactyl/resources/scripts/Pterodactyl_Ardhita_Theme.css
+    rm /var/www/pterodactyl/resources/scripts/index.tsx
+    mv index.tsx /var/www/pterodactyl/resources/scripts/index.tsx
+    mv Pterodactyl_Ardhita_Theme.css /var/www/pterodactyl/resources/scripts/Pterodactyl_Ardhita_Theme.css
+    cd /var/www/pterodactyl
+
+    curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+    apt update
+    apt install -y nodejs
+
+    npm i -g yarn
+    yarn
+
+    cd /var/www/pterodactyl
+    yarn build:production
+    sudo php artisan optimize:clear
+
+
+}
+
+installThemeQuestion(){
+    while true; do
+        read -p "Are you sure that you want to install the theme [y/n]? " yn
+        case $yn in
+            [Yy]* ) installTheme; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+repair(){
+    bash <(curl https://raw.githubusercontent.com/ardxryz/Pterodactyl_Ardhita_Theme/main/repair.sh)
+}
+
+restoreBackUp(){
+    echo "Restoring backup..."
+    cd /var/www/
+    tar -xvf Pterodactyl_Ardhita_Themebackup.tar.gz
+    rm Pterodactyl_Ardhita_Themebackup.tar.gz
+
+    cd /var/www/pterodactyl
+    yarn build:production
+    sudo php artisan optimize:clear
+}
+
+# Main script
+display_welcome
+check_token
+
+while true; do
+  clear
+echo -e "${BLUE}[+] =============================================== [+]${NC}"
+echo -e "${BLUE}[+]                     SELECT OPTION                    [+]${NC}"
+echo -e "${BLUE}[+] =============================================== [+]${NC}"
+echo -e "                                                       "
+echo -e "SELECT OPTION :"
+echo "1. Install theme"
+echo "2. Restore backup"
+echo "3. Repair panel (use if you have an error in the theme installation)"
+echo "x. Exit"
+
+read -p "Please enter a number: " choice
+if [ $choice == "1" ]
+    then
+    installThemeQuestion
+fi
+if [ $choice == "2" ]
+    then
+    restoreBackUp
+fi
+if [ $choice == "3" ]
+    then
+    repair
+fi
+if [ $choice == "x" ]
+    then
+    exit
+fi
